b723745167b1d35e5592fcf5d0607d4b29b7f507
박채영 <packcy030411@naver.com>
2026-04-10 09:57:00 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -2,36 +2,29 @@
 
 int main() {
     int arr[2][3] = {{1,2,3},{4,5,6}};
-    int (*p)[3] = arr;
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < 3; j++) {
-            printf("%d ", p[i][j]);
-        }
-        printf("\n");
+    int *ptr = arr[0];
+    for(int i = 0; i < 6; i++) {
+        printf("%d ", ptr[i]);
+        if ((i+1) % 3 == 0) printf("\n");
     }
     printf("\n");
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < 3; j++) {
-            printf("%d ", p[i][j]);
-        }
-        printf("\n");
+    for(int i = 0; i < 6; i++) {
+        printf("%d ", ptr[i]);
+        if ((i+1) % 3 == 0) printf("\n");
     }
     printf("\n");
-    p[1][1] = 0;
-    p[1][2] = 0;
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < 3; j++) {
-            printf("%d ", p[i][j]);
-        }
-        printf("\n");
+    ptr[3] = 0;
+    ptr[4] = 0;
+    ptr[5] = 0;
+    for(int i = 0; i < 6; i++) {
+        printf("%d ", ptr[i]);
+        if ((i+1) % 3 == 0) printf("\n");
     }
     printf("\n");
-    p[0][2] = 0;
-    for(int i = 0; i < 1; i++) {
-        for(int j = 0; j < 3; j++) {
-            printf("%d ", p[i][j]);
-        }
-        printf("\n");
+    ptr[2] = 0;
+    for(int i = 0; i < 3; i++) {
+        printf("%d ", ptr[i]);
     }
+    printf("\n");
     return 0;
 }
