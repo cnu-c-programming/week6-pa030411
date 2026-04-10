594b93dd98edae759a882d7d3edba0f70e8583b7
박채영 <packcy030411@naver.com>
2026-04-10 10:06:27 +0900

    Refactor array to two-dimensional and print addresses
    Changed the array from a one-dimensional to a two-dimensional array and updated the print statements to display the addresses of its elements.

--- code/3-3.c
+++ code/3-3.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int arr[6];
-    printf("%p\n", arr);
-    printf("%p\n", arr);
-    printf("%p\n", arr);
-    printf("%zu\n", sizeof(arr));
-    printf("%zu\n", sizeof(arr) / 2);
-    printf("%zu\n", sizeof(arr[0]));
+    int arr[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%p\n", (void *)&arr[i][j]);
+        }
+    }
+
     return 0;
 }
